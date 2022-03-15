667b6e13e15845942015cfc19894c2d130300fee
Morshaedul <morshaedulanirbanit@gmail.com>
2022-03-16 00:12:15 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// var wonNumber=0;
+// var lossNumber=0;
+
+// for(i=1; i<=5; i++){
+//     var gessNumber= parseInt(prompt("Enter a number 1 to 5: "));
+//     var randomNumber = Math.floor(Math.random()*5);
+
+//     if(randomNumber==gessNumber){
+//         console.log ("You are won")
+//         wonNumber++;
+//     }
+//     else{
+//         console.log ("You are lost your random number was:"+randomNumber)
+//         lossNumber++;
+
+//     }
+
+// }
+// document.write(" You are total won:"+wonNumber+ "<br>")
+// document.write(" You are total loss:"+lossNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var wonNumber=0;
+// var lossNumber=0;
+
+// for(i=1;i<=5; i++){
+//     var gessNumber =parseInt(prompt("Enter a number:"));
+// var randomNumber= Math.floor(Math.random()*5);
+// if(gessNumber==randomNumber){
+//     console.log("You win");
+//     wonNumber++
+
+// } else{
+//     console.log("You loss, your random number was :" + randomNumber)
+//     lossNumber++
+// }
+
+// }
+// document.write("Your total won number is:"+wonNumber+" </br>")
+// document.write("Your total loss number is:"+lossNumber)
+
+
+
+var data = new Date();
+var day=date.getFullYear()
+console.log(day)
+var year=date.getYear()
+console.log(year)
+
